7c492ab23756d892d970ab89657f37c5f6b30278
d-cern <83672965+d-cern@users.noreply.github.com>
2023-02-20 23:13:52 -0600

    read csv into list of point objects

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
 import os
+from Point import Point
+
+def ComputeConvexHull(points):
+
+
+def LowerTangent(a, b):
+    # a = rightmost point of A
+    # b = leftmost point of B
+    #
+    # while(T=ab is not lower tangent to both convex hulls):
+    #     while(T is not lower tangent to A):
+    #         a = a - 1
+    #
+    #     while(T is not lower tangent to B):
+    #         b = b + 1
+
+
 
 inputFileDir = '../data/input.csv'
 
@@ -8,5 +25,21 @@ if not os.path.exists(inputFileDir):
     quit()
 
 # open input file
-inputFile = open(inputFileDir, 'r')
+file = open(inputFileDir, 'r')
 
+points = []
+
+#Read each line of the input file.
+#Storing the x-coordinates into x.
+#Storing the y-coordinates into y.
+line = file.readline()
+while line:
+    coordinates = line.split(',')
+    points.append(Point(coordinates[0], coordinates[1]))
+    line = file.readline()
+
+file.close()
+
+
+# for i in points:
+#     print(i.x + ' ' + i.y)

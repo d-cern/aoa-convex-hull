169f6998c1aa762e2b4a8a730e2e58e161ffba07
d-cern <83672965+d-cern@users.noreply.github.com>
2023-02-21 00:20:49 -0600

    implemented Point.IsAboveLine()

--- src/Point.py
+++ src/Point.py
@@ -1,5 +1,11 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from Line import Line
 
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = float(x)
+        self.y = float(y)
+
+    def IsAboveLine(self, l: Line):
+        yLine = (l.slope * self.x) + l.y_intercept
+
+        return bool(self.y > yLine)
